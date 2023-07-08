888daca7076bcf34279731863d117ef371a844a1
EnigmaDoor <diego.mrnj@gmail.com>
2023-07-08 15:42:01 -0500

    [ADD] config handling

--- pkg/cli.go
+++ pkg/cli.go
@@ -1,34 +1,39 @@
 package scoreplay
 
 import (
+	"log"
 	"fmt"
 
+	"github.com/spf13/viper"
 	"github.com/urfave/cli/v2"
 )
 
 // Struct used to remember CLI options
 type Options struct {
 	// Used in CLI or interactive requested search
-	Competition string;
-	Season string;
-	Competitor string;
-	Player string;
+	Competition string
+	Season string
+	Competitor string
+	Player string
 
 	// Will we fetch from the API, or local read
-	Input string;
+	Input string
 	// Will we local write
-	Output string;
+	Output string
 
 	// api data
-	ApiKey string;
-	ApiRoute string;
-	ApiEnv string;
-	ApiVer string;
-	ApiLoc string;
+	ApiKey string `mapstructure:"API_KEY"`
+	ApiRoute string `mapstructure:"API_ROUTE"`
+	ApiEnv string `mapstructure:"API_ENV"`
+	ApiVer string `mapstructure:"API_VER"`
+	ApiLoc string `mapstructure:"API_LOC"`
 }
 
 func CLI(args []string) {
-	opts := LoadConf()
+	opts, err := LoadConf(); if err != nil {
+		// todo error handling
+		return
+	}
 
 	app := &cli.App{
 		Name: "scoreplay",
@@ -61,7 +66,8 @@ func CLI(args []string) {
 	}
 }
 
-func LoadConf() *Options {
+func LoadConf() (*Options, error) {
+	var err error
 	opts := Options{
 		ApiKey: "",
 		ApiRoute: "https://api.sportradar.com/soccer-extended",
@@ -69,5 +75,31 @@ func LoadConf() *Options {
 		ApiVer: "v4",
 		ApiLoc: "en",
 	}
-	return &opts
+
+	// First read the configs file
+	viper.AddConfigPath("./configs")
+	viper.SetConfigName("api")
+	viper.SetConfigType("env")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal("Error reading env file", err)
+	}
+	// Viper unmarshals the loaded env varialbes into the struct
+	if err := viper.Unmarshal(&opts); err != nil {
+		log.Fatal(err)
+	}
+
+	// And overwrite with the .env file
+	viper.AddConfigPath(".")
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal("Error reading env file", err)
+	}
+	// Viper unmarshals the loaded env varialbes into the struct
+	if err := viper.Unmarshal(&opts); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("OPTIONS", opts)
+	return &opts, err
 }
